aa9b0b8912e49110677a9dbdba2ba002fc775cd0
Gabriela <55298017+gmsmartinez@users.noreply.github.com>
2021-10-30 00:54:04 -0300

    Update App.py

--- App.py
+++ App.py
@@ -17,10 +17,10 @@ def start_tuitear():
         start = input("¿Queres ingresar tu usuario y contrasenia?: [S/N] ")
         start = start.upper()
         if start == "N":
-            print("En serio?? Somos mejores que facebook!!!")
+            print("En serio?? Somos mejores que Facebook!!!")
             exit()
         if start == "S":
-            print("Bien!! Vamos a tuiear!!!")
+            print("Bien!! Vamos a tuitear!!!")
 start_tuitear()
 
 usuario = Usuario(input("Bienvenido a Pdptwitter, por favor ingrese su nombre de usuario: "))
@@ -32,7 +32,8 @@ while (True):
     print("Bienvenido " + usuario.nombre + " a Pdptwitter, ¿que desea hacer?")
     print("1. Ver mis tweets")
     print("2. Escribir un tweet")
-    print("3. Cerrar sesion")
+    print("3. Ver mis Menciones")
+    print("4. Cerrar Sesion")
     
     opcion = int(input())
     if opcion == 1:
@@ -46,11 +47,30 @@ while (True):
         respuesta = usuario.twittear()
         print(respuesta)
 
+    
+
     elif opcion == 3:
+        
+        print("De cual usuario arrobado queres ver las menciones?")
+        if tweet in usuarioMaria.twittear:
+            print(tweet)
+        elif tweet in usuarioJuan.twittear:
+            print(tweet)
+        elif tweet in usuarioPedro.twittear:
+            print(tweet)
+        
         break
+
+    elif opcion == 4:
+        break
+
+
 
     else:
         print("Ingrese una opcion valida")
+
+
+
 
 # Terminator.guardarTweets(usuario.nombre, usuario.mensajes)
 Terminator.guardarTweets(usuarioPedro.nombre, usuarioPedro.mensajes)
